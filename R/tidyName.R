7c7445e7ee9b4aa8c35a8b8d62944462ff08b2a9
andrew-hipp <andrew.hipp@gmail.com>
2017-01-31 10:09:47 -0600

    Moving development functions to separate workspace Moves a number of functions that are in varying states of development to a separate workspace

--- R/tidyName.R
+++
@@ -1,5 +0,0 @@
-tidyName <- function(x, fixes = c('_', '.', ' ', '-', '>'), case = c('lower', 'upper', 'nochange')) {
-  x = switch(case[1], lower = tolower(x), upper = toupper(x), nochange = x)
-  for(i in fixes) x <- (gsub(i, "", x, fixed = T))
-  x
-  }
