d9545225ce849506ebd741b9797ca3b9d7d8833d
andrew-hipp <andrew.hipp@gmail.com>
2017-01-31 14:30:50 -0600

    updates filter.by first pass through update code on filter.by and documentation

--- R/filter.by.R
+++ R/filter.by.R
@@ -1,5 +1,5 @@
-filter.by <- function(dat, taxa, req,
-                      threshInds = 2, threshTaxa = 2,
+filter.by <- function(dat, taxa, req = NULL,
+                      threshInds, threshTaxa = 2,
                       use.tidyName = FALSE, ...) {
   ## returns just loci for which the requested taxa are present at some threshold
   ## default to returning 'all'
@@ -8,9 +8,11 @@ filter.by <- function(dat, taxa, req,
   if(class(dat) == 'pyRAD.loci') dat <- dat$radSummary
   if(use.tidyName) {
     row.names(dat$inds.mat) <- tidyName(row.names(dat$inds.mat), ...)
-	taxa <- tidyName(taxa, ...)
+	taxa <- sapply(taxa, tidyName, ...)
 	}
-  dat.mat <- dat$inds.mat[taxa, ] # this is the case if you pass in a summary.pyRAD.loci object
-  if(threshold == 'all') threshold <- length(taxa)
-  return(names(which(apply(dat.mat, 2, sum) >= threshold)))
+  dat.mat <- dat$inds.mat[unlist(unique(taxa)), ]
+  tax.thresh.mat <- t(sapply(taxa, function(x) colSums(dat.mat[x, ]) >= threshInds[x]))
+  if(length(req) > 0) dat.mat <- dat.mat[, which(colSums(dat.mat[req, ]) == length(req))]
+  out <- names(which(colSums(dat.mat) >= threshTaxa))
+  return(out)
   }
