726e2d4ce75472b4a692f4339e335d14192d4b5c
andrew-hipp <andrew.hipp@gmail.com>
2017-01-31 14:02:49 -0600

    Enhances filter.by Adds arguments to make filter.by more particular about how many individuals and taxa are required

--- R/filter.by.R
+++ R/filter.by.R
@@ -1,6 +1,9 @@
-filter.by <- function(dat, taxa, threshold = 'all', use.tidyName = FALSE, ...) {
+filter.by <- function(dat, taxa, req,
+                      threshInds = 2, threshTaxa = 2,
+                      use.tidyName = FALSE, ...) {
   ## returns just loci for which the requested taxa are present at some threshold
   ## default to returning 'all'
+  ## updated 2017-01-31 to allow you to subset by a threshold humber of individuals per taxon
   if(!class(dat) %in% c('summary.pyRAD.loci', 'pyRAD.loci')) stop("This function only works with summary.pyRAD.loci datatypes")
   if(class(dat) == 'pyRAD.loci') dat <- dat$radSummary
   if(use.tidyName) {
