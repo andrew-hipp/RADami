2b219df3e76c4032d4ae9c469579ee0bc97b1c44
andrew-hipp <andrew.hipp@gmail.com>
2017-01-31 16:03:17 -0600

    pairwise.fst - initial commit

---
+++ R/pairwise.fst.R
@@ -0,0 +1,29 @@
+pairwise.fst <- function(dat = loci.subsetted.3, group.list = taxa, to.do = c('alba', 'macrocarpa', 'bicolor', 'stellata', 'muehlenbergii'), do.hist = TRUE, cores = 1) {
+## returns pairwise FST list for groups of individuals
+## arguments
+##  dat = object of class subset.pyRAD.loci
+##  group.list = named list of character vectors, indicating groups of individuals
+##  to.do = which groups to generate pairwise data for
+##  do.hist = whether a histogram of pairwise FST should be written to the working directory
+##  cores = how many processing cores to use
+  fst <- gt <- gr <- list()
+  sp.pairs <- vector('list', 0)
+  for(i in 2:length(to.do)) {
+    for(j in seq(i-1)) {
+      message("DOING:")
+	  sp.pairs[[length(sp.pairs) + 1]] <- to.do[c(i, j)]
+	  message(paste(c(to.do[i], to.do[j]), collapse = ' - '))
+	  gr[[to.do[i]]][[to.do[j]]] <- group.subset.pyRAD.loci(dat, group.list[c(to.do[i], to.do[j])], mins = 2, cores = cores)
+	  gt[[to.do[i]]][[to.do[j]]] <- genotypes.pyRAD.loci(dat, group.list[c(to.do[i], to.do[j])], taxa = unlist(group.list[c(to.do[i], to.do[j])]), loci = row.names(gr[[to.do[i]]][[to.do[j]]]), cores = cores, na.rm = 'columns')
+      fst[[to.do[i]]][[to.do[j]]] <- mclapply(gt[[to.do[i]]][[to.do[j]]], function(x) try(wc(x), silent = TRUE), mc.cores=cores)
+      fst[[to.do[i]]][[to.do[j]]] <- fst[[to.do[i]]][[to.do[j]]][-which(sapply(fst[[to.do[i]]][[to.do[j]]], function(x) class(x) == 'try-error'))]
+	  if(do.hist) {
+	    pdf(paste('fst', to.do[i], to.do[j], 'pdf', format(Sys.time(), "%Y-%m-%d-%H.%M"), sep = '.'))
+	    hist(sapply(fst[[to.do[i]]][[to.do[j]]], function(x) x$FST), 20, xlab = 'FST', main = paste('Pairwise FST,', to.do[i], '-', to.do[j]))
+	    dev.off()
+		}
+	  } #close j
+	} #close i
+	out <- list(fst = fst, groups = gr, genotypes = gt, sp.pairs = sp.pairs, snpLocs = dat$snpLocs, timestamp = Sys.time())
+	return(out)
+	}
