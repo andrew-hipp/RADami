f7d7193c41a7145101ed683e63527b670caaa084
Andrew Hipp <andrew.hipp@gmail.com>
2018-08-17 11:34:27 -0500

    Fixing verbose in rad2phy... written the opposite way 'round

--- R/rad2phy.R
+++ R/rad2phy.R
@@ -7,7 +7,7 @@ function(pyDat, inds = row.names(pyDat), loci = dimnames(pyDat)[[2]], outfile = 
   open(outfile)
   cat(paste(length(inds), sum(sapply(pyDat[inds[1], loci], nchar)), "\n"), file = outfile) #header: number of individuals, number of bases
   for(i in inds) {
-    if(!verbose) message(paste("Writing DNA line for individual", i))
+    if(verbose) message(paste("Writing DNA line for individual", i))
 	cat(i, file = outfile)
 	cat(paste(rep(" ", padding - nchar(i)), collapse = ""), file = outfile)
 	cat(paste(pyDat[i, loci], collapse = ""), file = outfile)
