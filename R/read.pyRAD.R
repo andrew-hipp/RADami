55dc55ad2dce06d519a2e6acbdfb337aead61a19
Andrew Hipp <andrew.hipp@gmail.com>
2018-08-17 10:28:41 -0500

    Cleanup on read.pyRAD

--- R/read.pyRAD.R
+++ R/read.pyRAD.R
@@ -1,6 +1,5 @@
 read.pyRAD <-
-function(filename, reportInterval = 20000, breakLinesSeparate = FALSE, doSummary = TRUE,
-         removeNA = TRUE, ...) {
+function(filename, reportInterval = 20000, breakLinesSeparate = FALSE, doSummary = TRUE, ...) {
 ## reads the all.aligned file out of pyRAD, parses into names, loci, sequences
 ## updated with breakLinesSeparate in Oct 2012 because pyRAD switched to single-line summaries at the end of each aligned file
 ## updated 2012-11-16 to keep breaklines intact
